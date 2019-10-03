b3a82ca84cd67ce822f36f97ca0ab53980663300
Frederic Collonval <frederic.collonval@safrangroup.com>
2019-10-03 05:16:58 -0400

    Fix recipe for linux

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,5 @@
+REG QUERY HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\4.0 /v VCTargetsPath
+REG QUERY HKLM\SOFTWARE\Wow6432Node\Microsoft\MSBuild\ToolsVersions\4.0 /v VCTargetsPath
 cd wrappers\Python
 "%PYTHON%" setup.py install cmake=vc%VS_MAJOR%,%ARCH%
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
